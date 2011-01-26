a95aaedc84794f71543bb14df2412b503446e725
sasq64 <jonas.minnberg@gmail.com>
2011-01-26 14:03:28 +0100

    Added missing file

---
+++ src/com/ssb/droidsound/utils/Log.java
@@ -0,0 +1,26 @@
+package com.ssb.droidsound.utils;
+
+public class Log {
+	
+	public static final void d(String tag, String fmt, Object...args) {
+		String line = tag + ": " + String.format(fmt, args);
+		android.util.Log.d("Droidsound", line);
+	}
+	
+	public static final void d(String tag, String line) {
+		android.util.Log.d("Droidsound", tag + ": " + line);
+	}
+
+	public static final void w(String tag, String line) {
+		android.util.Log.w("Droidsound", tag + ": " + line);
+	}
+
+	public static final void w(String tag, String line, Throwable th) {
+		android.util.Log.w("Droidsound", tag + ": " + line, th);
+	}
+
+	public static final void i(String tag, String fmt, Object...args) {
+		String line = tag + ": " + String.format(fmt, args);
+		android.util.Log.i("Droidsound", line);
+	}
+}
