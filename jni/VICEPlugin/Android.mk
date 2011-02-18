7e6702bf0d251d7bc6d3ef8da7f6bd09a810321a
Antti S. Lankila <alankila@gmail.com>
2011-02-18 11:06:03 +0200

    Use -O3 for VICEPlugin.

--- jni/VICEPlugin/Android.mk
+++ jni/VICEPlugin/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := resid
 LOCAL_ARM_MODE := arm
-LOCAL_CFLAGS := -ffast-math -fno-exceptions
+LOCAL_CFLAGS := -O3 -ffast-math -fno-exceptions
 LOCAL_CPP_EXTENSION := cc
 MY_RESID_FILES = $(wildcard $(LOCAL_PATH)/vice/resid/*.cc)
 LOCAL_SRC_FILES := $(MY_RESID_FILES:$(LOCAL_PATH)%=%)
@@ -11,6 +11,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vice
 LOCAL_ARM_MODE := arm
+LOCAL_CFLAGS = -O3
 LOCAL_CPP_EXTENSION := cc
 LOCAL_SRC_FILES := \
 	VICEPlugin.cc \
