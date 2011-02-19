20a73f4760b2f362fdc4d59ff5fbb516dc7abc1b
Antti S. Lankila <alankila@gmail.com>
2011-02-19 20:18:19 +0200

    Speedup from Dag Lem

--- jni/VICEPlugin/vice/resid/filter.cc
+++ jni/VICEPlugin/vice/resid/filter.cc
@@ -139,7 +139,7 @@ static model_filter_init_t model_filter_init[2] = {
     sizeof(opamp_voltage_8580)/sizeof(*opamp_voltage_8580),
     // FIXME: Measure for the 8580.
     1.0,
-    0.5,  // FIXME: For now we pretend that the working point is 0V.
+    0.75,  // FIXME: For now we pretend that the working point is 0V.
     470e-12,
     12.18,
     1.31,
@@ -336,9 +336,9 @@ Filter::Filter()
     int Vddt = model_filter[0].Vddt;
 
     for (int i = 0; i < (1 << 16); i++) {
-      // The table index is right-shifted 16 times in order to fit in
-      // 16 bits; the argument to sqrt is thus multiplied by (1 << 16).
-      int Vg = Vddt - (int)(sqrt((float)i*(1 << 16)) + 0.5f);
+      // The table index is right-shifted 15 times in order to fit in
+      // 16 bits; the argument to sqrt is thus multiplied by (1 << 15).
+      int Vg = Vddt - (int)(sqrt((float)i*(1 << 15)) + 0.5f);
       if (Vg >= (1 << 16)) {
 	// Clamp to 16 bits.
 	// FIXME: If the DAC output voltage exceeds the max op-amp output
@@ -498,7 +498,7 @@ void Filter::set_w0()
 {
   model_filter_t& f = model_filter[sid_model];
   Vw = Vw_bias + f.f0_dac[fc];
-  Vw_term = (f.Vddt >> 1)*(f.Vddt >> 1) - (Vw >> 1)*(f.Vddt >> 1) + (Vw >> 1)*(Vw >> 2);
+  Vddt_Vw_2 = (unsigned(f.Vddt - Vw)*unsigned(f.Vddt - Vw) >> 1);
 
   // FIXME: w0 is temporarily used for MOS 8580 emulation.
   // MOS 8580 cutoff: 0 - 12.5kHz.
