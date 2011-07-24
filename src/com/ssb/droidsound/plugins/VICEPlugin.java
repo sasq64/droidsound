c8bd71de69ed04369bb82d7d8d4bc758fda323df
Lioncash <mathew1900@hotmail.com>
2011-07-24 00:05:13 -0400

    Updated VICE to 24071
    Original VICE changelog:
    
    "Added the new advantages of the vic20 io system to the c64 io system"
    
    NOTE: Code still compile correctly =D
    
    FUNFACT: Getting this to compile on the first try probably gave me the biggest dopamine spike I've ever had.

--- src/com/ssb/droidsound/plugins/VICEPlugin.java
+++ src/com/ssb/droidsound/plugins/VICEPlugin.java
@@ -209,6 +209,6 @@ public class VICEPlugin extends DroidSoundPlugin {
 	
 	@Override
 	public String getVersion() {
-		return "VICE 2.3.7-r24063, reSID 1.0-pre1";
+		return "VICE 2.3.7-r24071, reSID 1.0-pre1";
 	}
 }
