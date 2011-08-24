effcbe9950aedd0f04fb2ccbae518b8b79bbb5e8
Lioncash <mathew1900@hotmail.com>
2011-08-24 08:23:09 -0400

    Updated VICE to revision 24398

--- jni/VICEPlugin/vice/6510core.c
+++ jni/VICEPlugin/vice/6510core.c
@@ -570,7 +570,7 @@
       CLK_ADD(CLK, (clk_inc));                                                     \
                                                                                    \
       if (LOCAL_DECIMAL()) {                                                       \
-          tmp = (reg_a_read & 0xf) + (tmp_value & 0xf) + (reg_p & 0x1);            \
+          tmp = (reg_a_read & 0xf) + (tmp_value & 0xf) + LOCAL_CARRY();            \
           if (tmp > 0x9) {                                                         \
               tmp += 0x6;                                                          \
           }                                                                        \
@@ -579,7 +579,7 @@
           } else {                                                                 \
               tmp = (tmp & 0xf) + (reg_a_read & 0xf0) + (tmp_value & 0xf0) + 0x10; \
           }                                                                        \
-          LOCAL_SET_ZERO(!((reg_a_read + tmp_value + (reg_p & 0x1)) & 0xff));      \
+          LOCAL_SET_ZERO(!((reg_a_read + tmp_value + LOCAL_CARRY()) & 0xff));      \
           LOCAL_SET_SIGN(tmp & 0x80);                                              \
           LOCAL_SET_OVERFLOW(((reg_a_read ^ tmp) & 0x80)                           \
                               && !((reg_a_read ^ tmp_value) & 0x80));              \
@@ -588,7 +588,7 @@
           }                                                                        \
           LOCAL_SET_CARRY((tmp & 0xff0) > 0xf0);                                   \
       } else {                                                                     \
-          tmp = tmp_value + reg_a_read + (reg_p & P_CARRY);                        \
+          tmp = tmp_value + reg_a_read + LOCAL_CARRY();                            \
           LOCAL_SET_NZ(tmp & 0xff);                                                \
           LOCAL_SET_OVERFLOW(!((reg_a_read ^ tmp_value) & 0x80)                    \
                               && ((reg_a_read ^ tmp) & 0x80));                     \
@@ -648,7 +648,7 @@ be found that works for both.
       tmp = reg_a_read & (value);                                 \
       if (LOCAL_DECIMAL()) {                                      \
           tmp_2 = tmp;                                            \
-          tmp_2 |= (reg_p & P_CARRY) << 8;                        \
+          tmp_2 |= LOCAL_CARRY() << 8;                            \
           tmp_2 >>= 1;                                            \
           LOCAL_SET_SIGN(LOCAL_CARRY());                          \
           LOCAL_SET_ZERO(!tmp_2);                                 \
@@ -664,7 +664,7 @@ be found that works for both.
           }                                                       \
           reg_a_write = tmp_2;                                    \
       } else {                                                    \
-          tmp |= (reg_p & P_CARRY) << 8;                          \
+          tmp |= LOCAL_CARRY() << 8;                              \
           tmp >>= 1;                                              \
           LOCAL_SET_NZ(tmp);                                      \
           LOCAL_SET_CARRY(tmp & 0x40);                            \
@@ -1320,12 +1320,12 @@ be found that works for both.
 #define SBC(value, clk_inc, pc_inc)                                             \
   do {                                                                          \
       WORD src, tmp;                                                            \
+      unsigned int tmp_a;                                                       \
                                                                                 \
       src = (WORD)(value);                                                      \
       CLK_ADD(CLK, (clk_inc));                                                  \
       tmp = reg_a_read - src - ((LOCAL_CARRY()) ? 0 : 1);                       \
-      if (reg_p & P_DECIMAL) {                                                  \
-          unsigned int tmp_a;                                                   \
+      if (LOCAL_DECIMAL()) {                                                    \
           tmp_a = (reg_a_read & 0xf) - (src & 0xf) - ((LOCAL_CARRY()) ? 0 : 1); \
           if (tmp_a & 0x10) {                                                   \
               tmp_a = ((tmp_a - 6) & 0xf)                                       \
@@ -1340,13 +1340,13 @@ be found that works for both.
           LOCAL_SET_NZ(tmp & 0xff);                                             \
           LOCAL_SET_OVERFLOW(((reg_a_read ^ tmp) & 0x80)                        \
                              && ((reg_a_read ^ src) & 0x80));                   \
-          reg_a_write = (BYTE) tmp_a;                                           \
+          reg_a_write = (BYTE)tmp_a;                                            \
       } else {                                                                  \
           LOCAL_SET_NZ(tmp & 0xff);                                             \
           LOCAL_SET_CARRY(tmp < 0x100);                                         \
           LOCAL_SET_OVERFLOW(((reg_a_read ^ tmp) & 0x80)                        \
                              && ((reg_a_read ^ src) & 0x80));                   \
-          reg_a_write = (BYTE) tmp;                                             \
+          reg_a_write = (BYTE)tmp;                                              \
       }                                                                         \
       INC_PC(pc_inc);                                                           \
     }                                                                           \
--- src/com/ssb/droidsound/plugins/VICEPlugin.java
+++ src/com/ssb/droidsound/plugins/VICEPlugin.java
@@ -225,6 +225,6 @@ public final class VICEPlugin extends DroidSoundPlugin {
 	
 	@Override
 	public String getVersion() {
-		return "VICE 2.3.9-r24394, reSID 1.0-pre1";
+		return "VICE 2.3.9-r24398, reSID 1.0-pre1";
 	}
 }
