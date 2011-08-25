a7780c2cd03fe77e32f19dfcfed0031cf4005e34
Lioncash <mathew1900@hotmail.com>
2011-08-24 23:30:48 -0400

    Updated VICE to revision 24409

--- src/com/ssb/droidsound/plugins/VICEPlugin.java
+++ src/com/ssb/droidsound/plugins/VICEPlugin.java
@@ -225,6 +225,6 @@ public final class VICEPlugin extends DroidSoundPlugin {
 	
 	@Override
 	public String getVersion() {
-		return "VICE 2.3.9-r24401, reSID 1.0-pre1";
+		return "VICE 2.3.9-r24409, reSID 1.0-pre1";
 	}
 }
