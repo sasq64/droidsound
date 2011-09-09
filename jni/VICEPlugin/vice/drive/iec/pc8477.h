f5e42e88ff3c27b806bf45c6334d69e6f33ae8ba
lioncash <mathew1900@hotmail.com>
2011-09-09 07:53:11 -0400

    Updated VICE to revision 24538

---
+++ jni/VICEPlugin/vice/drive/iec/pc8477.h
@@ -0,0 +1,96 @@
+/*
+ * pc8477.h - dp8473/pc8477 emulation for the 1992 disk drive.
+ *
+ * Written by
+ *  Kajtar Zsolt <soci@c64.rulez.org>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef VICE_PC8477_H
+#define VICE_PC8477_H
+
+#include "types.h"
+#include "fdd.h"
+#include "alarm.h"
+
+struct disk_image_s;
+
+typedef enum pc8477_state_e {
+    PC8477_WAIT, PC8477_COMMAND, PC8477_READ, PC8477_WRITE, PC8477_EXEC, PC8477_RESULT
+} pc8477_state_t;
+
+typedef enum pc8477_cmd_e {
+    PC8477_CMD_MODE, PC8477_CMD_READ_A_TRACK, PC8477_CMD_SPECIFY, PC8477_CMD_SENSE_DRIVE_STATUS,
+    PC8477_CMD_WRITE_DATA, PC8477_CMD_RECALIBRATE, PC8477_CMD_SENSE_INTERRUPT,
+    PC8477_CMD_WRITE_DELETED_DATA, PC8477_CMD_READ_DATA, PC8477_CMD_SEEK, PC8477_CMD_SCAN_EQUAL,
+    PC8477_CMD_SCAN_LOW_OR_EQUAL, PC8477_CMD_SCAN_HIGH_OR_EQUAL, PC8477_CMD_SET_TRACK,
+    PC8477_CMD_READ_ID, PC8477_CMD_FORMAT_A_TRACK, PC8477_CMD_READ_DELETED_DATA,
+    PC8477_CMD_VERSION, PC8477_CMD_NSC, PC8477_CMD_DUMPREG, PC8477_CMD_PERPENDICULAR_MODE,
+    PC8477_CMD_INVALID
+} pc8477_cmd_t;
+
+typedef struct pc8477_s {
+    char *myname;
+    pc8477_cmd_t command;
+    pc8477_state_t state;
+    drive_context_t *mycontext;
+
+    /* Floppy drives */
+    fd_drive_t *fdds[4], *fdd;
+    int seeking[4];
+    int motor[4]; /* motor signals */
+    int sel;
+    int cmd_sel;
+    int irq;
+
+    CLOCK seek_clk;
+
+    /* Registers */
+    BYTE st[4];
+    int track[4]; /* actual track of head */
+    int step_rate, motor_off_time, motor_on_time, nodma;
+    int index_count;
+    int rate;
+
+    int sector; /* sector register */
+    int is8477; /* dp8473 or pc 8477 */
+
+    alarm_t *exec_alarm;
+    int bufp, buf_size;
+    BYTE buf[8192];
+    int cmdp, cmd_size;
+    BYTE cmd[9];
+    int resp, res_size;
+    BYTE res[7];
+} pc8477_t;
+
+struct drive_context_s;
+
+extern void pc8477d_init(drive_context_t *drv);
+extern void pc8477_setup_context(drive_context_t *drv);
+extern void pc8477d_store(drive_context_t *drv, WORD addr, BYTE byte);
+extern BYTE pc8477d_read(drive_context_t *drv, WORD addr);
+extern void pc8477d_reset(drive_context_t *drv);
+
+extern int pc8477_image_attach(pc8477_t *drv, struct disk_image_s *image);
+extern int pc8477_image_detach(pc8477_t *drv);
+
+#endif 
