966ac2f09e58b36644bb9e531e27b80fca0504a0
alankila <alankila@bel.fi>
2011-12-21 10:59:18 +0200

    Revert "Keep fixing, still broken..."
    This reverts commit d38611ad80e1b8e356824c024b2a879c0f074f62.

--- jni/UADEPlugin/uade/frontends/include/uade/uadeoptions.h
+++ jni/UADEPlugin/uade/frontends/include/uade/uadeoptions.h
@@ -1,5 +1,6 @@
+
 #define UADE_CONFIG_USER_MODE (1)
-#define UADE_CONFIG_BASE_DIR ""
-#define UADE_CONFIG_UADE_CORE ""
+#define UADE_CONFIG_BASE_DIR "/Users/alankila/share/uade2"
+#define UADE_CONFIG_UADE_CORE "/Users/alankila/lib/uade2/uadecore"
 #define UADE_CONFIG_HAVE_URANDOM
 #define UADE_VERSION "2.13"
--- src/com/ssb/droidsound/utils/Unzipper.java
+++ src/com/ssb/droidsound/utils/Unzipper.java
@@ -27,14 +27,13 @@ public class Unzipper {
 
 				OutputStream os = new FileOutputStream(out);
 				int data;
-				while (0 < (data = zis.read(buffer))) {
+				while (0 != (data = is.read(buffer))) {
 					os.write(buffer, 0, data);
 				}
 
 				zis.closeEntry();
 				os.close();
 			}
-			zis.close();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
