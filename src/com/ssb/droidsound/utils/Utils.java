26fb26d62edf3250f6996af0c0558e7a0eb5a92a
Jonas Minnberg <sasq64@gmail.com>
2012-10-31 22:20:31 +0100

    Fixes

---
+++ src/com/ssb/droidsound/utils/Utils.java
@@ -0,0 +1,60 @@
+package com.ssb.droidsound.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Context;
+
+public class Utils {
+
+	public static void removeDir(File dir) {		
+		File [] files = dir.listFiles();
+		if(files != null) {
+			for(File f : files) {
+				if(f.isFile())
+					f.delete();
+				else
+					removeDir(f);
+			}
+			dir.delete();
+		}		
+	}
+	
+	public static File dumpFile(File outFile, InputStream is) throws IOException {
+				
+		FileOutputStream os = new FileOutputStream(outFile);
+		byte[] buffer = new byte[1024 * 64];
+
+		int rc = 0;
+		while(rc >= 0) {
+			rc = is.read(buffer);
+			if(rc > 0) {
+				os.write(buffer, 0, rc);
+			}
+		}
+		os.close();
+		is.close();
+		
+		return outFile;
+	}
+
+	public static String readAsset(Context ctx, String name) {
+		InputStream is;
+		String s = null;
+		try {
+			is = ctx.getAssets().open(name);
+			byte [] data = new byte [is.available()];        
+	        is.read(data);
+	        is.close();
+	        s = new String(data, "ISO8859_1");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+	
+	
+
+}
